494f1e487e675abd600be0c84a94015d4fadb5ef
AppleDron <andriyesipov@gmail.com>
2023-10-28 20:59:20 +0300

    Types

--- dist/index.js
+++ dist/index.js
@@ -1,74 +1,84 @@
-// Simple types
-let total = 100;
-let named = "Bob";
-let isActive = false;
-let empty = null;
-let undf = undefined;
-// Неявний type
-let age = 10;
-age = 10;
-// type any
-let age1 = 12;
-age1 = "Lol";
-age1 = false;
-// type unknown
-// let name1: unknown = 'Andrew'
-// name1 = 12
-// name1.toFixed();
-// Array type
-const numbers = [1, 2, 3, 4, 5, 6];
-numbers.push(12);
-const user = {
-    name: "Andrew",
-    age: 12,
+// // Simple types
+// let total: number = 100;
+// let named: string = "Bob";
+// let isActive: boolean = false;
+// let empty: null = null;
+// let undf: undefined = undefined;
+const fruits = {
+    apples: 15,
+    banana: 30,
+    orange: 30,
 };
-user.age = "12";
-// const event: eventType = 'home'
-const evented = "lesson";
-// Enum
-var Sizes;
-(function (Sizes) {
-    Sizes["small"] = "small";
-    Sizes["medium"] = "medium";
-    Sizes["large"] = "large";
-})(Sizes || (Sizes = {}));
-const button = "large";
-const button2 = Sizes.large;
-// function types
-// return something
-// function add(num1: number, num2: number): number {
-//   return num1 + num2;
-// }
-// console.log(add(2, 3));
-// Without return - void
-function add(num1, num2) {
-    console.log(num1 + num2);
-}
-add(2, 3);
-function greating(user) {
-    console.log(`Hello, ${user.name}`);
-}
-function userConstructor(name, age, hobby) {
-    return {
-        name,
-        age,
-        hobby,
-    };
-}
-const Car = {
-    color: "red",
-    price: 1000,
-    currency: "UAH",
-    start(color) {
-        console.log("Start" + this.color);
-    },
+const frozenGoods = {
+    iceCream: 25,
+    fish: 30,
+    berry: 10,
 };
-const user3 = {
-    name: "bob",
-    age: 12,
+const books = {
+    books: 20,
+    magazines: "not found",
 };
-const admin = {
+const Pens = {
+    pens: 23,
+    pencils: 19,
+    highlighters: "not found",
+};
+// Generics
+function identity(args) {
+    return args;
+}
+let output1 = identity("This is my string");
+let output2 = identity(12);
+// Створіть загальну функцію reverse, яка приймає масив будь-якого типу і повертає масив у зворотньому порядку
+function reverse(arr) {
+    return arr.reverse();
+}
+const numbers = [1, 2, 3, 4, 5];
+console.log(reverse(numbers));
+const strings = ["One", "two", "three", "four", "five"];
+console.log(reverse(strings));
+// extends or keyof
+function lengthOfObject(obj) {
+    return obj.length;
+}
+lengthOfObject({ film: "Earth", length: 5 });
+// Створіть загальну функцію getProperty, яка приймає об'єкт та ключ як рядок.
+// Фнкція повинна повертати значення цього ключа з об'єкта
+const getProperty = (obj, key) => {
+    return obj[key];
+};
+const student = {
     name: "John",
-    age: 23,
-    role: "Admin",
+    age: 25,
 };
+console.log(getProperty(student, "name"));
+const updateTodo = (todo, fieldsToUpdate) => {
+    return { ...todo, ...fieldsToUpdate };
+};
+const todo1 = {
+    title: "Learn TypeScript",
+    describtion: "Study the basics of TypeScript",
+    completed: false,
+};
+const updatedTodo = updateTodo(todo1, {
+    completed: true,
+    describtion: "I learned TypeScript",
+});
+console.log(updatedTodo);
+const john = {
+    name: "John",
+    age: 30,
+};
+// john.age = 31;  - помилка
+const numbersArr = [1, 2, 3, 4, 5];
+const person = {
+    name: "John",
+    age: 26,
+    // adress: "Ukraine",
+};
+const database = {
+    Kyiv: 2884000,
+    Kharkiv: 1441000,
+    Odesa: 1015000,
+};
+database.Lviv = 721301;
--- src/index.ts
+++ src/index.ts
@@ -1,122 +1,278 @@
-// Simple types
-let total: number = 100;
-let named: string = "Bob";
-let isActive: boolean = false;
-let empty: null = null;
-let undf: undefined = undefined;
+// // Simple types
+// let total: number = 100;
+// let named: string = "Bob";
+// let isActive: boolean = false;
+// let empty: null = null;
+// let undf: undefined = undefined;
 
-// Неявний type
-let age = 10;
-age = 10;
+// // Неявний type
+// let age = 10;
+// age = 10;
 
-// type any
-let age1: any = 12;
-age1 = "Lol";
-age1 = false;
+// // type any
+// let age1: any = 12;
+// age1 = "Lol";
+// age1 = false;
 
-// type unknown
-// let name1: unknown = 'Andrew'
-// name1 = 12
-// name1.toFixed();
+// // type unknown
+// // let name1: unknown = 'Andrew'
+// // name1 = 12
+// // name1.toFixed();
 
-// Array type
-const numbers: number[] = [1, 2, 3, 4, 5, 6];
-numbers.push(12);
+// // Array type
+// const numbers: number[] = [1, 2, 3, 4, 5, 6];
+// numbers.push(12);
 
-// Object type
-type User = {
-  name: string;
-  age: number | string;
-};
-const user: User = {
-  name: "Andrew",
-  age: 12,
-};
-user.age = "12";
+// // Object type
+// type User = {
+//   name: string;
+//   age: number | string;
+// };
+// const user: User = {
+//   name: "Andrew",
+//   age: 12,
+// };
+// user.age = "12";
 
-// Custom type
-type eventType = "lesson" | "deadline";
-// const event: eventType = 'home'
-const evented: eventType = "lesson";
+// // Custom type
+// type eventType = "lesson" | "deadline";
+// // const event: eventType = 'home'
+// const evented: eventType = "lesson";
 
-// Union type
-type size = "small" | "medium" | "large";
+// // Union type
+// type size = "small" | "medium" | "large";
 
-// Enum
-enum Sizes {
-  small = "small",
-  medium = "medium",
-  large = "large",
-}
-const button: size = "large";
-const button2: Sizes = Sizes.large;
+// // Enum
+// enum Sizes {
+//   small = "small",
+//   medium = "medium",
+//   large = "large",
+// }
+// const button: size = "large";
+// const button2: Sizes = Sizes.large;
 
-// function types
-// return something
-// function add(num1: number, num2: number): number {
-//   return num1 + num2;
+// // function types
+// // return something
+// // function add(num1: number, num2: number): number {
+// //   return num1 + num2;
+// // }
+
+// // console.log(add(2, 3));
+
+// // Without return - void
+// function add(num1: number, num2: number): void {
+//   console.log(num1 + num2);
 // }
 
-// console.log(add(2, 3));
+// add(2, 3);
 
-// Without return - void
-function add(num1: number, num2: number): void {
-  console.log(num1 + num2);
+// type User1 = {
+//   name: string;
+// };
+
+// function greating(user: User1): void {
+//   console.log(`Hello, ${user.name}`);
+// }
+
+// type User2 = {
+//   name: string;
+//   age: number;
+//   hobby: string;
+// };
+
+// function userConstructor(name: string, age: number, hobby: string): User2 {
+//   return {
+//     name,
+//     age,
+//     hobby,
+//   };
+// }
+
+// type Car = {
+//   color: string;
+//   price: number;
+//   currency: string;
+//   start: (color: string) => {};
+// };
+
+// const Car = {
+//   color: "red",
+//   price: 1000,
+//   currency: "UAH",
+//   start(color) {
+//     console.log("Start" + this.color);
+//   },
+// };
+
+// type UserType = {
+//   name: string;
+//   age: number;
+//   role?: string;
+// };
+
+// const user3: UserType = {
+//   name: "bob",
+//   age: 12,
+// };
+
+// const admin: UserType = {
+//   name: "John",
+//   age: 23,
+//   role: "Admin",
+// };
+
+// Index Properties
+type Goods = {
+  [key: string]: number;
+};
+
+const fruits: Goods = {
+  apples: 15,
+  banana: 30,
+  orange: 30,
+};
+
+const frozenGoods: Goods = {
+  iceCream: 25,
+  fish: 30,
+  berry: 10,
+};
+
+// Визначте інтерфейс для об'єкта, де ключем є рядок, а значення може бути рядок або число.
+// Створіть декілька об'єктів цього типу.
+
+type ShoppingItems = {
+  [key: string]: number | string;
+};
+
+const books: ShoppingItems = {
+  books: 20,
+  magazines: "not found",
+};
+
+const Pens: ShoppingItems = {
+  pens: 23,
+  pencils: 19,
+  highlighters: "not found",
+};
+
+// Generics
+function identity<T>(args: T): T {
+  return args;
 }
 
-add(2, 3);
+let output1 = identity<string>("This is my string");
+let output2 = identity<number>(12);
 
-type User1 = {
-  name: string;
-};
+// Створіть загальну функцію reverse, яка приймає масив будь-якого типу і повертає масив у зворотньому порядку
 
-function greating(user: User1): void {
-  console.log(`Hello, ${user.name}`);
+function reverse<T>(arr: T[]): T[] {
+  return arr.reverse();
 }
 
-type User2 = {
-  name: string;
-  age: number;
-  hobby: string;
-};
+const numbers: number[] = [1, 2, 3, 4, 5];
+console.log(reverse(numbers));
 
-function userConstructor(name: string, age: number, hobby: string): User2 {
-  return {
-    name,
-    age,
-    hobby,
-  };
+const strings: string[] = ["One", "two", "three", "four", "five"];
+console.log(reverse(strings));
+
+// extends or keyof
+function lengthOfObject<T extends { length: number }>(obj: T): number {
+  return obj.length;
 }
 
-type Car = {
-  color: string;
-  price: number;
-  currency: string;
-  start: (color: string) => {};
+lengthOfObject({ film: "Earth", length: 5 });
+
+// Створіть загальну функцію getProperty, яка приймає об'єкт та ключ як рядок.
+// Фнкція повинна повертати значення цього ключа з об'єкта
+
+const getProperty = <T, K extends keyof T>(obj: T, key: K): T[K] => {
+  return obj[key];
 };
 
-const Car = {
-  color: "red",
-  price: 1000,
-  currency: "UAH",
-  start(color) {
-    console.log("Start" + this.color);
-  },
-};
-
-type UserType = {
-  name: string;
-  age: number;
-  role?: string;
-};
-
-const user3: UserType = {
-  name: "bob",
-  age: 12,
-};
-
-const admin: UserType = {
+const student = {
   name: "John",
-  age: 23,
-  role: "Admin",
+  age: 25,
 };
+
+console.log(getProperty(student, "name"));
+
+// Patrial<T>
+type Todo = {
+  title: string;
+  describtion: string;
+  completed: boolean;
+};
+
+const updateTodo = (todo: Todo, fieldsToUpdate: Partial<Todo>): Todo => {
+  return { ...todo, ...fieldsToUpdate };
+};
+
+const todo1: Todo = {
+  title: "Learn TypeScript",
+  describtion: "Study the basics of TypeScript",
+  completed: false,
+};
+
+const updatedTodo = updateTodo(todo1, {
+  completed: true,
+  describtion: "I learned TypeScript",
+});
+console.log(updatedTodo);
+
+// Readonly
+type User = {
+  name: string;
+  age: number;
+};
+
+const john: Readonly<User> = {
+  name: "John",
+  age: 30,
+};
+
+// john.age = 31;  - помилка
+
+const numbersArr: ReadonlyArray<number> = [1, 2, 3, 4, 5];
+
+// numbersArr.push(6); - error
+
+// Pick<T,K>
+// type Person = {
+//   name: string;
+//   age: number;
+//   adress: string;
+// };
+
+// type PersonSummary = Pick<Person, "name" | "age">;
+
+// const person: PersonSummary = {
+//   name: "Andrew",
+//   age: 26,
+// };
+
+// Omit<T, K>
+type Person = {
+  name: string;
+  age: number;
+  adress: string;
+};
+
+type PersonWithounAdress = Omit<Person, "adress">;
+
+const person: PersonWithounAdress = {
+  name: "John",
+  age: 26,
+  // adress: "Ukraine",
+};
+
+// Record<K, T>
+type CityDataBase = Record<string, number>;
+
+const database: CityDataBase = {
+  Kyiv: 2884000,
+  Kharkiv: 1441000,
+  Odesa: 1015000,
+};
+
+database.Lviv = 721301;
